3eabc19abb64ce366e8418aa6c2591768728580b
aa-tan <a.tan@keio.jp>
2019-04-18 16:04:12 +0900

    Added basic flask routing for backend

--- back/setup.py
+++ back/setup.py
@@ -1,1 +1,20 @@
-print("Setup")
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/post", methods = ['POST'])
+def post_route():
+    if request.method == 'POST':
+        authenticate(request.form)
+        print(request.data)
+        return "True"
+    elif request.method == 'GET':
+        return "get"
+def authenticate(body):
+    pass
