097f220a8a43a876101b0c95a6fc6ecb61cdb135
aa-tan <a.tan@keio.jp>
2019-04-19 15:08:23 +0900

    Added script for handling user/pass speed metric

---
+++ front/src/public/scripts/script.js
@@ -0,0 +1,30 @@
+var sum = 0;
+var username_timestamps = [];
+var password_timestamps = [];
+
+function keyPressed(ele){
+    if(ele == "username"){
+        username_timestamps.push(Date.now());
+    }
+    else if(ele == "password"){
+        password_timestamps.push(Date.now());
+    }
+}
+
+function formSubmit(){
+    document.getElementById("speedUser").value = calculateSpeed("username")
+    document.getElementById("speedPass").value = calculateSpeed("password")
+    document.getElementById("login_form").submit()
+}
+
+function calculateSpeed(type){ 
+    var timestamps;
+    if(type == "username"){
+        timestamps = username_timestamps
+    }
+    else if(type == "password"){
+        timestamps = password_timestamps
+    }
+    // Last keypress - first keypress / amount of keypresses
+    return (timestamps[timestamps.length-1] - timestamps[0]) / timestamps.length
+}
