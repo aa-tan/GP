becc8b55cf3ada2d841ab6a37d395a4ef447e614
aa-tan <a.tan@keio.jp>
2019-04-23 11:41:21 +0900

    Renamed to app.js

--- front/src/app.js
+++ front/src/app.js
@@ -4,10 +4,7 @@ var bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// MongoDB variable declarations
-var MongoClient = require('mongodb').MongoClient;
 var path = require('path');
-const url = "mongodb://localhost:27017/gp"
 var viewPath = path.join(__dirname, 'views');
 
 // Setting up Express
@@ -15,7 +12,7 @@ app.set('views', viewPath);
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('src/public')) 
+app.use(express.static('src/public'))
 //app.use(express.cookieSession());
 
 app.use(require('./controllers/routes'))
