7441e5dfc22e0fb5c775c0ffd580ad159e22e54f
aa-tan <a.tan@keio.jp>
2019-04-24 16:24:08 +0900

    Helper function for password generation

--- back/util.py
+++ back/util.py
@@ -1,6 +1,13 @@
 import json
+import random
 def bytes_to_object(bytestring):
     string = bytestring.decode("utf-8")
     obj = json.loads(string)
     return obj
 
+def random_password():
+    with open("./10-million-password-list-top-1000.txt") as f:
+        lines = f.readlines()
+        partone = random.choice(lines)[:-1]
+        parttwo = random.choice(lines)[:-1]
+        return partone+parttwo
