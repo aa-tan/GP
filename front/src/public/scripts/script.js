36b5be5fd7370ec4d2985024f985dc18db0b9627
aa-tan <a.tan@keio.jp>
2019-04-26 12:12:51 +0900

    Added function to capture mouse movement

--- front/src/public/scripts/script.js
+++ front/src/public/scripts/script.js
@@ -1,6 +1,10 @@
 var sum = 0;
 var username_timestamps = [];
 var password_timestamps = [];
+var timestamp = null;
+var last_mouse_x = null;
+var last_mouse_y = null;
+var typingSpeed = 0;
 
 function keyPressed(ele){
     if(ele == "username"){
@@ -12,23 +16,50 @@ function keyPressed(ele){
 }
 
 function focusForm(){
-    document.getElementById("username").focus()
+    document.getElementById("username").focus();
 }
 
 function formSubmit(){
-    document.getElementById("speedUser").value = calculateSpeed("username")
-    document.getElementById("speedPass").value = calculateSpeed("password")
-    document.getElementById("login_form").submit()
+    document.getElementById("speedUser").value = calculateTypingSpeed("username");
+    document.getElementById("speedPass").value = calculateTypingSpeed("password");
+    document.getElementById("speedMouse").value = calculateMouseSpeed();
+    document.getElementById("login_form").submit();
 }
 
-function calculateSpeed(type){ 
-    var timestamps;
+function calculateTypingSpeed(type){ 
+    var typingTimestamps;
     if(type == "username"){
-        timestamps = username_timestamps
+        typingTimestamps = username_timestamps;
     }
     else if(type == "password"){
-        timestamps = password_timestamps
+        typingTimestamps = password_timestamps;
     }
     // Last keypress - first keypress / amount of keypresses
-    return (timestamps[timestamps.length-1] - timestamps[0]) / timestamps.length
+    return (typingTimestamps[typingTimestamps.length-1] - typingTimestamps[0]) / typingTimestamps.length
 }
+
+function calculateMouseSpeed(){
+    var mouseSum = 0;
+    for(var i = 0; i < speed.length; i++){
+        mouseSum += speed[i]; 
+    }
+    return mouseSum/speed.length
+}
+
+document.body.addEventListener("mousemove", (event) => {
+    if(timestamp == null){
+        timestamp = Date.now();
+        last_mouse_x = event.screenX;
+        last_mouse_y = event.screenY;
+    } 
+
+    var now = Date.now();
+    var dt = now - timestamp;
+    var dx = event.screenX - last_mouse_x;
+    var dy = event.screenY - last_mouse_y;
+    var distance = Math.sqrt(Math.pow(dx)+Math.pow(dy));
+    typingSpeed = distance / dt;
+    timestamp = now;
+    lastMouseX = event.screenX;
+    lastMouseY = event.screenY;
+})
