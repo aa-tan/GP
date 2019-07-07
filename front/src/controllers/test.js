4fb66124c684a7bbfbdbc3b70e9bcb549b5826ae
Aaron Tan <a.tan@keio.jp>
2019-07-07 05:14:36 +0000

    Updated input views with cookie tracker

--- front/src/controllers/test.js
+++ front/src/controllers/test.js
@@ -6,7 +6,16 @@ var router = express.Router();
 
 // Home page
 router.get('/', (req, res) => {
-    res.render('input_form', {title:'hey', message:'Testing Mode', submitPath:"/login"});
+    var success = req.cookies.success
+    var failure = req.cookies.failure
+    if(success == undefined || failure == undefined){
+	res.cookie("success", 0)
+	res.cookie("failure", 0)	
+    	res.render('input_form', {title:'hey', message:'Testing Mode', submitPath:"/login"});
+    }
+    else{
+	res.render('input_form', {title:'hey', message:'Success: '+success+" Failure: "+failure, submitPath:"/login"})
+    }
 })
 
 
@@ -14,17 +23,26 @@ router.get('/', (req, res) => {
 router.post('/login', (req, res)=> {
     name = req.body.username;
     password = req.body.password;
+    var success = req.cookies.success
+    var failure = req.cookies.failure
     console.log(req.body)
     axios.post(`http://${process.env.BACK_SERVICE}:${process.env.BACK_PORT}/authenticate`, req.body)
     .then((response) => {
         if(response['data'] == 'True'){
+	    if(success != undefined && failure != undefined){
+		res.cookie("success", parseInt(success) + 1)
+	    }
             res.redirect('/success')
         }
         else if(response['data'] == 'False'){
+	    if(success != undefined && failure != undefined){
+		console.log("adding fail cookie")
+		res.cookie("failure", parseInt(failure) + 1)
+	    }
             res.redirect('/failure')
         }
         else if(response['data'] == 'Invalid'){
-            res.redirect('invalid')
+            res.redirect('/invalid')
         }
     })
     .catch((error) => {
